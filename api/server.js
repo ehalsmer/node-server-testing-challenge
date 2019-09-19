14fbb926c383b1d7cd449e9f8f5d4ee0fc2de844
Eleasah Halsmer <eleasahjh@gmail.com>
2019-09-19 15:10:50 -0600

    Endpoint and route for /api/users

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const usersRouter = require('../users/users-router');
 
 const server = express();
 
@@ -7,5 +8,5 @@ server.use(express.json());
 server.get('/', (req, res) => {
     res.status(200).json({message: "server up!"})
 });
-
+server.use('/api/users', usersRouter)
 module.exports = server;
